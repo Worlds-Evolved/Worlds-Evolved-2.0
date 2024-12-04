7cecfc3f70b01e56825d744bf7334b606cf35ebd
devin <devin.lighgheart@gmail.com>
2024-12-04 15:26:53 -0700

    corrected file name of navbar in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./NavBar";
+import NavBar from "./navbar";
 import Home from "./pages/home"; 
 
 const App = () => {
