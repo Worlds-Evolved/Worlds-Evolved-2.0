3a2c5da01320390756147973a9727379c823b7ff
matt k <matt.kovach91@gmail.com>
2024-12-07 17:17:58 -0600

    added a console.log to get lat/log for placing markers

--- src/components/mymap.jsx
+++ src/components/mymap.jsx
@@ -1,10 +1,8 @@
-import { MapContainer, ImageOverlay } from 'react-leaflet';
+import { MapContainer, ImageOverlay, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 
 const MyMap = () => {
   const imageUrl = "https://d.img.vision/capstone/1000001374_(2).png";
-
-
   const bounds = [
     [0, 0],
     [5500, 8192],
@@ -16,15 +14,28 @@ const MyMap = () => {
         crs={L.CRS.Simple}
         bounds={bounds}
         style={{ height: "100%", width: "100%" }}
-        zoom={-1} // Start zoomed out
-        minZoom={-2} // Allow zooming further out
-        maxZoom={2} // Limit how far you can zoom in
-        zoomSnap={0.5} // Enable finer zoom levels
+        zoom={-2} // Start zoomed out
+        minZoom={-3} // Allow zooming further out
+        maxZoom={2} // Limit zooming in
+        zoomSnap={0.1} // Very smooth zoom increments
+        zoomDelta={0.1} // Smooth zoom buttons
+        scrollWheelZoom={true} // Enable scroll wheel zoom
+        wheelPxPerZoomLevel={100} // Smooth scrolling response
       >
         <ImageOverlay url={imageUrl} bounds={bounds} />
+        <CaptureClick />
       </MapContainer>
     </div>
   );
+};
+
+const CaptureClick = () => {
+  useMapEvents({
+    click(event) {
+      console.log("Coordinates:", event.latlng);
+    }
+  });
+  return null;
 };
 
 export default MyMap;
