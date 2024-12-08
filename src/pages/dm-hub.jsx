cf3fbd1044cfc9f715bf1ec4478adb9e981c9adc
devin <devin.lighgheart@gmail.com>
2024-12-08 08:17:47 -0700

    used fetchuserdetails and created fecthCampaignDetails. created basic return statemnt, needs more funtionality added

---
+++ src/pages/dm-hub.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { getCampaignDetails, getUserDetails } from "../api/api";
+
+const campaignId = 3
+const DmHub = () => {
+
+  const [userDetails, setUserDetails] = useState(null);
+  const [campaignDetails, setcampaignDetails] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) return; 
+
+    const fetchUserDetails = async () => {
+      try {
+        const username = await getUserDetails(token);
+        setUserDetails(username);
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+
+    const fetchCampaignDetails = async () => {
+      try {
+        const campaignDetails = await getCampaignDetails(campaignId, token);
+        setcampaignDetails(campaignDetails);
+      } catch (error) {
+        console.error("Error fetching campaigns:", error);
+      }
+    };
+
+    fetchUserDetails();
+    fetchCampaignDetails();
+  }, []); 
+ 
+  
+  return (
+    <div className="dm-hub-page">
+      <div className="dm-hub-container">
+        <h1>Dungeon Master's Hun</h1>
+        
+
+
+        <h2></h2>
+      
+          <div className="campaign-details">
+            {campaignDetails.map((campaigndetail) => (
+              <div key={campaigndetail.id} className="campaign-detail">
+                <h3>{campaignDetail.title}</h3>
+                <h3>{campaigndetail.players}</h3>
+
+                <button>Invite</button>
+                <button>Add Note</button>
+                <p>{campaignDetail.description}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+
+export default DmHub;
