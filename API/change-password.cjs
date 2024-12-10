1fddd2daf6ea11c277179089a9059d0d93d4d16d
matt k <matt.kovach91@gmail.com>
2024-12-10 08:22:47 -0600

    authenticateToken function

--- API/change-password.cjs
+++ API/change-password.cjs
@@ -3,3 +3,18 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const prisma = require('../prisma/index.cjs')
 const router = express.Router();
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.slice(7);
+
+  if (!token){
+    return next();
+  }
+
+  try {
+    
+  } catch (error) {
+    
+  }
+}
