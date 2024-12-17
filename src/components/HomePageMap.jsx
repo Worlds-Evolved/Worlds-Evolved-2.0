e55a71e368f0864c3fb1728a617f527169756689
matt k <matt.kovach91@gmail.com>
2024-12-17 08:40:45 -0600

    small adjustments to home page styling

--- src/components/HomePageMap.jsx
+++ src/components/HomePageMap.jsx
@@ -12,12 +12,18 @@ const HomeMap = () => {
     [0, 0],  // Southwest coordinates
     [5500, 8192]  // Northeast coordinates
   ];
+
+  const center = [
+    (bounds[0][0] + bounds[1][0]) / 2,
+    (bounds[0][1] + bounds[1][1]) / 2,
+  ];
   
   return (
     <div className="home-map-container">
       <MapContainer
         crs={L.CRS.Simple}
         bounds={bounds}
+        center={center}
         style={{ height: "100%", width: "100%", margin: 0, padding: 0 }}
         zoom={-2} // Start zoomed out
         minZoom={-3} // Allow zooming further out
